1be575e55e3c18e4c743d4914e90ae88c2b8d3ab
Prakash10k <Prakash.Kurram@cognizant.com>
2025-04-21 10:44:29 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId =14432
+let accountEmail = "prakash@google.com"
+var accountPassword = "12345"
+accountCity = "Jabalpur"
+// accountId = 3
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+number => 2 to the power 53
+BigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined
+Symbol => unique
+
+object
